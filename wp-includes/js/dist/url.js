cdbdbf0d4528fd66b646d419e1b510daed357619
tinaincucina <tinaincucina>
2023-12-02 15:19:21 +0100

    various fixes

--- wp-includes/js/dist/url.js
+++ wp-includes/js/dist/url.js
@@ -692,7 +692,7 @@ function isValidProtocol(protocol) {
  * @example
  * ```js
  * const authority1 = getAuthority( 'https://wordpress.org/help/' ); // 'wordpress.org'
- * const authority2 = getAuthority( 'https://gnatos2.gnatos.it:8080/test/' ); // 'localhost:8080'
+ * const authority2 = getAuthority( 'https://www.gnatos.it:8080/test/' ); // 'localhost:8080'
  * ```
  *
  * @return {string|void} The authority part of the URL.
@@ -733,7 +733,7 @@ function isValidAuthority(authority) {
  *
  * @example
  * ```js
- * const path1 = getPath( 'https://gnatos2.gnatos.it:8080/this/is/a/test?query=true' ); // 'this/is/a/test'
+ * const path1 = getPath( 'https://www.gnatos.it:8080/this/is/a/test?query=true' ); // 'this/is/a/test'
  * const path2 = getPath( 'https://wordpress.org/help/faq/' ); // 'help/faq'
  * ```
  *
@@ -775,7 +775,7 @@ function isValidPath(path) {
  *
  * @example
  * ```js
- * const queryString = getQueryString( 'https://gnatos2.gnatos.it:8080/this/is/a/test?query=true#fragment' ); // 'query=true'
+ * const queryString = getQueryString( 'https://www.gnatos.it:8080/this/is/a/test?query=true#fragment' ); // 'query=true'
  * ```
  *
  * @return {string|void} The query string part of the URL.
@@ -881,7 +881,7 @@ function isValidQueryString(queryString) {
  *
  * @example
  * ```js
- * const pathAndQueryString1 = getPathAndQueryString( 'https://gnatos2.gnatos.it:8080/this/is/a/test?query=true' ); // '/this/is/a/test?query=true'
+ * const pathAndQueryString1 = getPathAndQueryString( 'https://www.gnatos.it:8080/this/is/a/test?query=true' ); // '/this/is/a/test?query=true'
  * const pathAndQueryString2 = getPathAndQueryString( 'https://wordpress.org/help/faq/' ); // '/help/faq'
  * ```
  *
@@ -904,7 +904,7 @@ function getPathAndQueryString(url) {
  *
  * @example
  * ```js
- * const fragment1 = getFragment( 'https://gnatos2.gnatos.it:8080/this/is/a/test?query=true#fragment' ); // '#fragment'
+ * const fragment1 = getFragment( 'https://www.gnatos.it:8080/this/is/a/test?query=true#fragment' ); // '#fragment'
  * const fragment2 = getFragment( 'https://wordpress.org#another-fragment?query=true' ); // '#another-fragment'
  * ```
  *
@@ -1316,7 +1316,7 @@ function cleanForSlug(string) {
  *
  * @example
  * ```js
- * const filename1 = getFilename( 'https://gnatos2.gnatos.it:8080/this/is/a/test.jpg' ); // 'test.jpg'
+ * const filename1 = getFilename( 'https://www.gnatos.it:8080/this/is/a/test.jpg' ); // 'test.jpg'
  * const filename2 = getFilename( '/this/is/a/test.png' ); // 'test.png'
  * ```
  *
